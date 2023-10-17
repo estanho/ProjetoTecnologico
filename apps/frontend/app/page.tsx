19d1b68782614fbd329ad290ffe529c1c96c06c5
estanho <estanhoeu@gmail.com>
2023-10-17 03:28:07 -0300

    corrigindo erro vercel

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import LogoutButton from '../components/LogoutButton';
 import DeployButton from '../components/DeployButton';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Index() {
   const supabase = createServerComponentClient({ cookies });
 
