47e385cf70b047b34f7d488861f389f1f35507d1
estanho <estanhoeu@gmail.com>
2023-10-17 23:00:52 -0300

    Retirando console.log

--- apps/frontend/app/auth/callback/route.ts
+++ apps/frontend/app/auth/callback/route.ts
@@ -8,8 +8,6 @@ export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const code = searchParams.get('code');
 
-  console.log(cookieStore);
-
   if (code) {
     await supabase.auth.exchangeCodeForSession(code);
   }
