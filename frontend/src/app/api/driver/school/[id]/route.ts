dcf98c65374a4fb51090da430cfdf022bcdd99b9
estanho <estanhoeu@gmail.com>
2023-11-21 00:05:17 -0300

    at

--- frontend/src/app/api/driver/school/[id]/route.ts
+++ frontend/src/app/api/driver/school/[id]/route.ts
@@ -118,7 +118,7 @@ export async function PATCH(request: Request) {
       throw new Error(result.data.message);
     }
   } catch (err) {
-    return NextResponse.json({ error: true });
+    return NextResponse.json({ error: true, messsage: err });
   }
 
   return NextResponse.json({ error: false });
