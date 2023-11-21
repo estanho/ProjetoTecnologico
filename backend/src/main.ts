23d3391782aabbb94a3281601c6974fce9055ceb
estanho <estanhoeu@gmail.com>
2023-11-20 22:43:47 -0300

    Alteração nos links

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://microrota.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://microrota.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
   });
