9a39d6285e0d4f9ba3d6c17bc64c5ad35271351a
estanho <estanhoeu@gmail.com>
2023-11-30 11:17:36 -0300

    Teste

--- frontend/src/app/responsible/map/[id]/page.tsx
+++ frontend/src/app/responsible/map/[id]/page.tsx
@@ -1,12 +1,17 @@
-import { redirect } from 'next/navigation';
 import Map from './Map';
 import NavBar from '../../../../components/NavBar';
-import supabaseServer from '../../../utils/supabaseServer';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export const dynamic = 'force-dynamic';
 
 export default async function NewRoutePage({ params }: any) {
+  const supabase = createServerComponentClient({ cookies });
+
   const {
     data: { session },
-  } = await supabaseServer().auth.getSession();
+  } = await supabase.auth.getSession();
 
   if (!session) {
     return redirect('/auth/login');
--- frontend/src/app/student/map/[id]/page.tsx
+++ frontend/src/app/student/map/[id]/page.tsx
@@ -3,6 +3,8 @@ import NavBar from '../../../../components/NavBar';
 import { redirect } from 'next/navigation';
 import supabaseServer from '../../../utils/supabaseServer';
 
+export const dynamic = 'force-dynamic';
+
 export default async function NewRoutePage({ params }: any) {
   const {
     data: { session },
