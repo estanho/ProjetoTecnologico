2ab0a45662f7fa85ab07015f21976a4fd4fbe620
estanho <estanhoeu@gmail.com>
2023-11-30 22:07:37 -0300

    teste

--- frontend/src/app/api/driver/trip/route.ts
+++ frontend/src/app/api/driver/trip/route.ts
@@ -3,9 +3,6 @@ import axios from 'axios';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 export async function GET() {
   try {
     const supabase = createRouteHandlerClient({ cookies });
